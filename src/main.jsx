71eaaf44fb8a6ae1bc3dd7f9510369d7ed46a327
Miguel <miguelfuentes2101@gmail.com>
2025-10-23 13:50:25 -0500

    Usando bolt y supabase

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://zypyviskemdvvqnjnnui.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
