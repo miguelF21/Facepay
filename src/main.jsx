7fb816f205a0a36801de28f453eea6ce8b98f525
Miguel <miguelfuentes2101@gmail.com>
2025-10-23 14:10:46 -0500

    eliminando supabase para que lo haga bolt

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://zypyviskemdvvqnjnnui.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
