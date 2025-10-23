630a8d6b3d3d61e750685085c462c07033e46331
Miguel <miguelfuentes2101@gmail.com>
2025-10-23 18:30:27 -0500

    Commit usando supabase en vite

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://zypyviskemdvvqnjnnui.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -12,6 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
     domain={domain}
+    supabase={supabase}
     clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin,
