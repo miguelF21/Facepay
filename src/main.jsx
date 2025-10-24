73d521a487ea933aadf92a291e169b03567ce63d
Miguel <miguelfuentes2101@gmail.com>
2025-10-23 19:59:11 -0500

    Error de supakey solucionado, pero aun no conecta con la bd

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; 
-import App from './App';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Auth0Provider } from '@auth0/auth0-react'
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
+<React.StrictMode>
   <Auth0Provider
     domain={domain}
-    supabase={supabase}
     clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
-  >
-    <App />
+    >
+    <React.Suspense fallback={<div>Cargando...</div>}>
+      <App />
+    </React.Suspense>
   </Auth0Provider>
-);
+</React.StrictMode>,
+)
